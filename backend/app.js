3111d3ca5dbdeeae927b4e0fc36e617247abe26d
FocusCookie <stephan_lueddemann@web.de>
2019-11-09 09:31:43 +0100

    added helmet and startup to clean app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const config = require("config");
 
 const app = express();
 
+require("./startup/middleware")(app);
+
 const server = app.listen(config.get("server.port"), function() {
 	console.log(`Server runs: http://localhost:${config.get("server.port")}`);
 });
