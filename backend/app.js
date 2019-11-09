94a5cf15c94792e5fa320e2857d461c06e23bf58
FocusCookie <stephan_lueddemann@web.de>
2019-11-09 09:40:44 +0100

    set up morgan

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 require("./startup/middleware")(app);
 
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
 const server = app.listen(config.get("server.port"), function() {
 	console.log(`Server runs: http://localhost:${config.get("server.port")}`);
 });
--- backend/startup/middleware.js
+++ backend/startup/middleware.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
+const morgan = require("morgan");
 
 module.exports = function(app) {
 	app.use(express.json());
 	app.use(helmet());
+	app.use(morgan("dev"));
 };
