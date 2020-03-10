ef19fda3db0b91818c8147ce812de0248b89e9c3
FocusCookie <stephan_lueddemann@web.de>
2020-03-10 11:04:45 +0100

    Fixed icon problems, via adding Icons with npm and add the usage of it in plugins/vuetify config

--- frontend/sync/src/main.js
+++ frontend/sync/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import "@mdi/font/css/materialdesignicons.css";
 import { store } from "./store/store";
 import { ApiService } from "./services/api.service";
 
